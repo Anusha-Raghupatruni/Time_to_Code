13c6929930800c27ae14a426670549170452bada
Anusha-Raghupatruni <57944743+Anusha-Raghupatruni@users.noreply.github.com>
2021-06-21 23:32:05 +0530

    finding number of zeroes in N!

---
+++ Trailing_Zeroes_Factorial.py
@@ -0,0 +1,20 @@
+class Solution:
+    def trailingZeroes(self, N):
+        if(N < 0):
+            return -1
+        count = 0
+        while(N >= 5):
+            N //= 5
+            count += N 
+        return count
+
+
+
+
+t = int(input())
+for _ in range(t):
+        N = int(input())
+        ob = Solution()
+        ans = ob.trailingZeroes(N)
+        print(ans)
+
